5224e6ec64d10efec5931005bbbca2834ebdf4a4
Wazir Robinson <robinson82425@gmail.com>
2019-05-27 00:58:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,34 +1,34 @@
-// function basicTeenager(age) {
-//   if (age >= 13 && age <= 19) {
-//     return "You are a teenager!";
-//   }
-// }
-//
-//
-// function teenager(age) {
-//   if (age >= 13 && age <= 19) {
-//     return "You are a teenager!";
-//   } else {
-//     return "You are not a teenager";
-//   }
-//
-// }
-//
-//  function ageChecker(age) {
-//    if (age >= 13 && age <= 19) {
-//      return "You are a teenager!";
-//    } else if (age <= 12) {
-//      return "You are a kid";
-//    } else {
-//      return "You are a grouwnup";
-//    }
-//
-//  }
-//
-// function ternaryTeenager(age) {
-// return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
-//
-// }
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
+}
+
+
+function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenager";
+  }
+
+}
+
+ function ageChecker(age) {
+   if (age >= 13 && age <= 19) {
+     return "You are a teenager!";
+   } else if (age <= 12) {
+     return "You are a kid";
+   } else {
+     return "You are a grouwnup";
+   }
+
+ }
+
+function ternaryTeenager(age) {
+return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
+
+}
 
 function switchAge(age) {
   switch (age) {
